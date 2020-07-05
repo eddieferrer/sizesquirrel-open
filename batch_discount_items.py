10ee57db6535923e363e9274c70217c04190ede9
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2020-07-05 12:49:39 -0700

    Fixed arguments in batch jobs

--- batch_discount_items.py
+++ batch_discount_items.py
@@ -8,22 +8,22 @@ else:
     process = 'production_manage.py'
 
 argument_list = [
-    '--feed=adidas_outdoor --sendDiscountItems=True', 
-    '--feed=backcountry --sendDiscountItems=True',
-    '--feed=blackdiamondequipment --sendDiscountItems=True',
-    '--feed=bentgate --sendDiscountItems=True', 
-    '--feed=campsaver --sendDiscountItems=True',     
-    '--feed=ems --sendDiscountItems=True', 
-    '--feed=lasportiva --sendDiscountItems=True', 
-    '--feed=leftlanesports --sendDiscountItems=True',     
-    '--feed=moosejaw --sendDiscountItems=True', 
-    '--feed=rei --sendDiscountItems=True',     
-    '--feed=theclymb --sendDiscountItems=True'
+    '--feed=adidas_outdoor', 
+    '--feed=backcountry',
+    '--feed=blackdiamondequipment',
+    '--feed=bentgate', 
+    '--feed=campsaver',     
+    '--feed=ems', 
+    '--feed=lasportiva', 
+    '--feed=leftlanesports',     
+    '--feed=moosejaw', 
+    '--feed=rei',     
+    '--feed=theclymb'
 ]
 
 currentDate = datetime.datetime.today()
 print(currentDate)
-print("Processing Missing Items...")
+print("Processing Discount Items...")
 for arguments in argument_list:
-    subprocess.call(['python', process, 'process_feeds', arguments])
+    subprocess.call(['python', process, 'process_feeds', arguments, '--sendDiscountItems=True'])
     print("Finished:" + arguments)
