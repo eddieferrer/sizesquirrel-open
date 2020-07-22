1376a452e7bd23f8fbd09d7e36c34cb6f1478e8b
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2020-07-21 21:07:56 -0700

    Improved performance and workbox strategies

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -2,41 +2,37 @@
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
-  // Wait until window has loaded to register service worker
-  // to optimize load time for first visits
-  window.addEventListener('load', () => {
-    register(`${process.env.BASE_URL}service-worker.js`, {
-      ready() {
-        console.log('Service worker is active.');
-      },
-      registered(registration) {
-        console.log('Service worker has been registered.');
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+      console.log('Service worker is active.');
+    },
+    registered(registration) {
+      console.log('Service worker has been registered.');
 
-        // Routinely check for app updates by testing for a new service worker.
-        setInterval(() => {
-          registration.update();
-        }, 1000 * 60 * 60); // hourly checks
-      },
-      cached() {
-        console.log('Content has been cached for offline use.');
-      },
-      updatefound() {
-        console.log('New content is downloading.');
-      },
-      updated(registration) {
-        console.log('New content is available; please refresh.');
+      // Routinely check for app updates by testing for a new service worker.
+      setInterval(() => {
+        registration.update();
+      }, 1000 * 60 * 60); // hourly checks
+    },
+    cached() {
+      console.log('Content has been cached for offline use.');
+    },
+    updatefound() {
+      console.log('New content is downloading.');
+    },
+    updated(registration) {
+      console.log('New content is available; please refresh.');
 
-        // Add a custom event and dispatch it.
-        // Used to display of a 'refresh' banner following a service worker update.
-        // Set the event payload to the service worker registration object.
-        document.dispatchEvent(new CustomEvent('swUpdated', { detail: registration.waiting }));
-      },
-      offline() {
-        console.log('No internet connection found. App is running in offline mode.');
-      },
-      error(error) {
-        console.error('Error during service worker registration:', error);
-      },
-    });
+      // Add a custom event and dispatch it.
+      // Used to display of a 'refresh' banner following a service worker update.
+      // Set the event payload to the service worker registration object.
+      document.dispatchEvent(new CustomEvent('swUpdated', { detail: registration.waiting }));
+    },
+    offline() {
+      console.log('No internet connection found. App is running in offline mode.');
+    },
+    error(error) {
+      console.error('Error during service worker registration:', error);
+    },
   });
 }
