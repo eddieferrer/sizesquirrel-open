7fe7eb0a0818a97ef95f32c8fae3313d3460564a
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2020-07-21 21:07:56 -0700

    Improved performance and workbox strategies

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,4 +1,12 @@
 /* eslint-disable no-restricted-globals */
+
+// config
+workbox.setConfig({
+  debug: false,
+});
+
+// workbox.core.clientsClaim();
+
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
 self.addEventListener('message', (event) => {
@@ -8,8 +16,46 @@ self.addEventListener('message', (event) => {
   }
 });
 
-workbox.core.clientsClaim();
+// apply precaching. In the built version, the precacheManifest will
+// be imported using importScripts (as is workbox itself) and we can
+// precache this. This is all we need for precaching
+workbox.precaching.precacheAndRoute([]);
 
-// The precaching code provided by Workbox.
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(/\/apiv2/),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'api',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
