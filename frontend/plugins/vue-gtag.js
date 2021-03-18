90643a7d63f794058eaf3401a47fbcb401af833d
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2021-03-18 15:38:36 -0700

    Fixed gtag conversion

--- frontend/plugins/vue-gtag.js
+++ frontend/plugins/vue-gtag.js
@@ -5,17 +5,15 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default ({ app }) => {
   const getGDPR = localStorage.getItem('GDPR:accepted');
-  if (isProd) {
-    Vue.use(
-      VueGtag,
-      {
-        config: { id: 'UA-75492234-4' },
-        bootstrap: getGDPR === 'true',
-        appName: 'APP_NAME',
-        enabled: getGDPR === 'true',
-        pageTrackerScreenviewEnabled: true,
-      },
-      app.router
-    );
-  }
+  Vue.use(
+    VueGtag,
+    {
+      config: { id: 'UA-75492234-4' },
+      bootstrap: getGDPR === 'true',
+      appName: 'APP_NAME',
+      enabled: getGDPR === 'true' && isProd,
+      pageTrackerScreenviewEnabled: true,
+    },
+    app.router
+  );
 };
